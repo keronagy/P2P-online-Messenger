dd9aea002342cc22a9f1598106d0c00a6d57cd91
Kyrillos Nagy <keronagy@users.noreply.github.com>
2018-04-13 02:08:17 +0200

    GUI Changes

--- src/onlinep2pmessenger/FXMLDocumentController.java
+++ src/onlinep2pmessenger/FXMLDocumentController.java
@@ -11,6 +11,7 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 /**
  *
@@ -20,6 +21,9 @@ public class FXMLDocumentController implements Initializable {
     
     @FXML
     private Label label;
+    @FXML
+    private TextField searchTxt;
+    
     
     @FXML
     private void handleButtonAction(ActionEvent event) {
@@ -30,6 +34,7 @@ public class FXMLDocumentController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
+       // searchTxt.setStyle("-fx-text-fill: white;");
     }    
     
 }
