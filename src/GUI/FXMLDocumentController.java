cea71b9d431a0eef89f35e65d69a446241b8ebd7
Kord <k96.kord@gmail.com>
2018-04-20 06:47:10 +0200

    .

--- src/GUI/FXMLDocumentController.java
+++ src/GUI/FXMLDocumentController.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package onlinep2pmessenger;
+package GUI;
 
 import java.io.File;
 import java.io.FileInputStream;
