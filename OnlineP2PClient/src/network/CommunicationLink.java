20a010f0a0cab0a7ac977b1b988c10c45dd79f1a
Ahmed1337 <ahmedhamed523@gmail.com>
2018-05-03 16:21:40 +0200

    divided the project into 2 projects (client and server)

---
+++ OnlineP2PClient/src/network/CommunicationLink.java
@@ -0,0 +1,72 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package network;
+
+import java.io.*;
+import java.net.Socket;
+import java.util.HashMap;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import utility.CallbackOnReceiveHandler;
+
+/**
+ *
+ * @author fadia
+ */
+public class CommunicationLink extends Thread {
+
+    private CallbackOnReceiveHandler callBackHandler;
+    private ObjectInputStream ois;
+    private ObjectOutputStream oos;
+
+    /**
+     * @return the id
+     */
+
+    private CommunicationLink(CallbackOnReceiveHandler callBackHandler, Socket s) {
+        this.callBackHandler = callBackHandler;
+        try {
+            this.oos = new ObjectOutputStream(s.getOutputStream());
+            this.ois = new ObjectInputStream(s.getInputStream());
+        } catch (IOException ex) {
+            Logger.getLogger(CommunicationLink.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+    }
+
+    
+    
+    //CommunicationLink factory to run code after object construction
+    public static CommunicationLink generateCommunicationLink(CallbackOnReceiveHandler callBackHandler, Socket s) {
+        CommunicationLink cl = new CommunicationLink(callBackHandler, s);
+        cl.start();
+        return cl;
+    }
+
+    @Override
+    public void run() {
+        while (true) {
+            HashMap<String, String> received;
+            try {
+                received = (HashMap<String, String>) ois.readObject();
+                callBackHandler.handleReceivedData(received);
+            } catch (IOException ex) {
+                Logger.getLogger(CommunicationLink.class.getName()).log(Level.SEVERE, null, ex);
+            } catch (ClassNotFoundException ex) {
+                Logger.getLogger(CommunicationLink.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+    }
+
+    public void send(HashMap<String, String> msg) {
+        try {
+            oos.writeObject(msg);
+            oos.flush();
+        } catch (IOException ex) {
+            Logger.getLogger(CommunicationLink.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+}
---
+++ OnlineP2PServer/src/utility/Constants.java
@@ -0,0 +1,65 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package utility;
+
+/**
+ *
+ * @author fadia
+ */
+public class Constants {
+
+    public static final String SERVERIP = "127.0.0.1";
+    public static final int SERVERPORT = 2500;
+    public static final String ADDNEWCLIENTORDER = "handleNewClient";
+    public static final String ADDNEWROOMORDER = "handleNewRoom";
+    public static final String ADDNEWCLIENTTOROOMORDER = "handleClientAddedtoRoom";
+    public static final String REMOVECLIENTFROMROOMORDER = "handleClientRemovedFromRoom";
+    public static final String CONFIRMCREATEROOMORDER = "handleRoomCreated";
+    public static final String CONFIRMJOINROOMORDER = "handleRoomJoined";
+    public static final String CONFIRMLEAVEROOMORDER = "handleRoomLeft";
+
+    public static final String INITSTATUS = "online";
+    public static final String MAINCONNECTION = "serverConn";
+    public static final String ROOMJOINCONNECTION = "handleRoomJoin";
+    public static final String ROOMCREATECONNECTION = "handleRoomCreate";
+    public static final String ROOMSENDMESSAGE = "handleRoomMessage";
+    public static final String ROOMLEAVECONNECTION = "handleRoomLeave";
+    public static final String PEERMESSAGE = "peermessage";
+    public static final String TYPINGSTATUS = "typingstatus";
+    public static final String SEENTIME = "seentime";
+    public static final int P2PPORT = 5555;
+
+    //used to indicate the end of sending a data structure's content
+    public static final String EOF = "end";
+    //used to indicate the entity is a room
+    public static final String isARoom = "r";
+    //used to indicate the entity is a client
+    public static final String isAClient = "c";
+    //used to indicate the entity is a server
+    public static final String isAServer = "s";
+
+    //public static final String SENDERIDATTR = "senderid";
+    public static final String CLIENTIDATTR = "CID";
+    public static final String CLIENTSTATUSATTR = "CSTATUS";
+    public static final String CLIENTNAMEATTR = "CNAME";
+    public static final String CLIENTIPATTR = "CIP";
+    public static final String REQUESTTYPEATTR = "reqtype";
+    public static final String REPLYTYPEATTR = "reptype";
+    public static final String UPDATETYPEATTR = "ut";
+    public static final String ROOMIDATTR = "RID";
+    public static final String ROOMNAMEATTR = "RNA";
+
+    public static final String MESSAGEFROM = "mfrom";
+
+    public static final String MESSAGEFROMROOM = "handleMessageFromRoom";
+    public static final String MESSAGEFROMCLIENT = "handleMessageFromPeer";
+    public static final String TYPINGHANDLE = "handleTyping";
+    public static final String SEENHANDLE = "handleSeen";
+    public static final String ISTYPING = " is typing...";
+    public static final String NOTTYPING = "";
+    public static final String MESSAGE = "msg";
+
+}
