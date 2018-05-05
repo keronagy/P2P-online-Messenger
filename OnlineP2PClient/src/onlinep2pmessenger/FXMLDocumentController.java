1df5f2e24bb1b3440737d419cf8fc921d18a5e2f
Ahmed1337 <ahmedhamed523@gmail.com>
2018-05-05 20:06:12 +0200

    Fixed a bug

--- OnlineP2PClient/src/onlinep2pmessenger/FXMLDocumentController.java
+++ OnlineP2PClient/src/onlinep2pmessenger/FXMLDocumentController.java
@@ -223,12 +223,11 @@ public class FXMLDocumentController implements Initializable {
             kk.setSpacing(5);
             kk.setPadding(new Insets(5));
             EmojiesPopupVbox.getChildren().add(kk);
-            
 
         }
 
         EmojiesPopUp.setPopupContent(EmojiesPopupVbox);
-        
+
         emojies.setOnMouseClicked(e -> showEmojis(e));
         AddRoomBtn.setOnMouseClicked(e -> AddRoomDialog());
     }
@@ -403,13 +402,13 @@ public class FXMLDocumentController implements Initializable {
             JFXPopup CirclePopUp = new JFXPopup();
             if (adminID.equals(hamed.getId()) || hamed.isAdmin()) {
                 JFXButton RemoveMember = new JFXButton("Kick Member");
-                RemoveMember.setOnMouseClicked(e ->
-                {
+                RemoveMember.setOnMouseClicked(e
+                        -> {
                     hamed.kickClientFromRoom(RoomID, UserID);
                     CirclePopUp.hide();
-                        });
+                });
                 BtnsPop.getChildren().add(RemoveMember);
-                
+
             }
 
             CirclePopUp.setPopupContent(BtnsPop);
@@ -932,9 +931,13 @@ public class FXMLDocumentController implements Initializable {
             String clientName = clients.get(clientID).getName();
             SimpleStringProperty clientStatus = clients.get(clientID).getStatus();
             String roomID = msg.get(Constants.ROOMIDATTR);
-            String adminID = ((CustomStackPane) GroupTabVboxes.get(roomID)).getAdminID();
+
             //GUI add client to room
-            Platform.runLater(() -> EnterRoomUserCircle(clientName, clientID, clientStatus, roomID, adminID));
+            Platform.runLater(() -> {
+                String adminID = ((CustomStackPane) GroupTabVboxes.get(roomID)).getAdminID();
+                EnterRoomUserCircle(clientName, clientID, clientStatus, roomID, adminID);
+
+            });
         }
 
         public void handleClientRemovedFromRoom(HashMap<String, String> msg) {
